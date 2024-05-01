e4691799fbb93abb34368d560b934a1b4f3cd661
Marcelo Schreiber <marcelorissette15@gmail.com>
2024-05-01 15:34:50 -0300

    Refactor pullImages function to use async/await syntax in pull-images.ts

--- pull-images.ts
+++ pull-images.ts
@@ -2,15 +2,19 @@ import { $ } from "bun";
 
 import { codeConfig } from "./src/utils/createContainerConfig";
 
-let key: keyof typeof codeConfig;
 
-for (key in codeConfig) {
-  const image = codeConfig[key].image;
-  try {
-    $`docker pull ${codeConfig[key].image}`;
-  } catch (error) {
-    console.error(`Error pulling image: ${image}`, error);
+async function pullImages() {
+  let key: keyof typeof codeConfig;
+
+  for (key in codeConfig) {
+    const image = codeConfig[key].image;
+    try {
+      await $`docker pull ${codeConfig[key].image}`;
+    } catch (error) {
+      console.error(`Error pulling image: ${image}`, error);
+    }
   }
 }
 
+pullImages();
 // Path: pull-images.ts
